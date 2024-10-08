e94d4c73c0993feb990dd87e84436d5049e2db3b
Huy Viet Nguyen <huy-viet.nguyen@polymtl.ca>
2024-10-08 18:22:17 -0400

    ajout générateur et génération aléatoire du voisinage

--- Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
+++ Devoir2_INF8175_A24/RechercheLocale/code/solver_advanced.py
@@ -4,20 +4,7 @@ import random
 from schedule import Schedule
 import solver_naive
 
-# TODO: implémenter un itérateur pour le voisinage
-class Neighborhood():
-    def __init__(self, initial_solution, max_neighborhood_size: int=3000):
-        self.solution = initial_solution
-        self.max_size = max_neighborhood_size
-
-    def __iter__(self): 
-        return self
-    
-    def __next__(self):
-        return
-
-
-def get_neighborhood(solution: dict, timeout: lambda: bool, max_neighborhood_size: int=3000) -> list[dict]:
+def get_neighborhood(solution: dict, keys: list[str], timeout: lambda: bool, max_neighborhood_size: int=100):
     """Retourne un générateur du voisinage d'une solution et d'une certaine taille maximale. 
     Chaque élément du voisinage va différer d'au maximum un crénau par rapport à la solution donnée. 
     Si la fonction timeout retroune vrai, aucun voisinage est retourné et l'exécution s'arrête
@@ -27,23 +14,21 @@ def get_neighborhood(solution: dict, timeout: lambda: bool, max_neighborhood_siz
     """
     if timeout(): return []
 
-    neighborhood = []
-    shuffled_courses = list(solution.keys())
-    random.shuffle(shuffled_courses)
-
-    for course in shuffled_courses:
-        for other_course in shuffled_courses:
+    counter = 0
+    for course in keys:
+        for other_course in keys:
             if timeout(): return []
 
-            if len(neighborhood) > max_neighborhood_size:
-                return neighborhood
+            if counter < max_neighborhood_size:
+                neighbor = solution.copy()
+                neighbor[other_course] = solution[course]
+                
+                counter += 1
 
-            neighbor = solution.copy()
-            neighbor[other_course] = solution[course]
-            neighborhood.append(neighbor)
+                yield neighbor
+            else:
+                return
     
-    return neighborhood
-
 def get_valid_neighbors(neighboors: list[dict], schedule: Schedule, timeout: lambda: bool) -> list[dict]:
     valid_neighbors = []
 
@@ -72,18 +57,22 @@ def select(neighbors: list[dict], schedule: Schedule, timeout: lambda: bool) -> 
 def eval(solution: dict, schedule: Schedule) -> int:
     return schedule.get_n_creneaux(solution)
 
-def get_timer(duration: int = 600, time_margin: int = 5):
+def get_timer(duration: int = 300, time_margin: int = 5):
     assert duration > time_margin
 
     start = time.time()
     return lambda: time.time() - start > duration - time_margin
 
-def local_search(schedule, initial_solution: dict, timeout: lambda: bool, stagnation_counter: int=10):
+def local_search(schedule, initial_solution: dict, timeout: lambda: bool, stagnation_counter: int=20):
     best_solution = initial_solution
     counter = 0
+    iteration = 0
+    start = time.time()
     
     while not timeout():
-        neighborhood = get_neighborhood(best_solution, timeout)
+        keys = list(best_solution.keys())
+        random.shuffle(keys)
+        neighborhood = get_neighborhood(best_solution, keys, timeout)
         valid_neighbors = get_valid_neighbors(neighborhood, schedule, timeout)
         current_solution = select(valid_neighbors, schedule, timeout)
 
@@ -96,6 +85,10 @@ def local_search(schedule, initial_solution: dict, timeout: lambda: bool, stagna
 
             if counter >= stagnation_counter:
                 return best_solution
+            
+        iteration += 1
+
+    print("Temps moyen d'itération:", (time.time() - start) / iteration)
         
     return best_solution
 
@@ -106,11 +99,11 @@ def solve(schedule: Schedule):
     :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
     """
     # Add here your agent
-    timeout = get_timer(120, 20)
+    timeout = get_timer()
 
     naive_solution = solver_naive.solve(schedule)
     best_solution = naive_solution
-    
+
     while not timeout():
         solution = local_search(schedule, naive_solution, timeout)
 
